8dacc94c77202eb514e9db8d5e6e3941774afe6e
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-03-30 10:42:01 +0900

    add testcase

--- test/vote.ts
+++ test/vote.ts
@@ -1,7 +1,7 @@
 /* eslint-disable new-cap */
 import { expect, use } from 'chai'
 import { describe } from 'mocha'
-import { Contract, ethers, Wallet } from 'ethers'
+import { Contract, ethers, Wallet, BigNumber } from 'ethers'
 import { deployContract, MockProvider, solidity } from 'ethereum-waffle'
 import { mine } from '@devprotocol/util-ts'
 import Vote from '../build/Vote.json'
@@ -11,6 +11,8 @@ use(solidity)
 
 describe('Vote', () => {
 	const VOTING_BLOCK = 100
+	const options0 = ethers.utils.formatBytes32String('http://hogehoge/0')
+	const options1 = ethers.utils.formatBytes32String('http://hogehoge/1')
 	const init = async (): Promise<
 		[Contract, Contract, MockProvider, number, Wallet[]]
 	> => {
@@ -18,6 +20,11 @@ describe('Vote', () => {
 		const wallets = provider.getWallets()
 		const voteEmitter = await deployContract(wallets[0], VoteEmitter)
 		const vote = await deployContract(wallets[0], Vote, [
+			'subject',
+			ethers.utils.formatBytes32String('body'),
+			[options0, options1],
+			'dummy-body-mime-type',
+			'dummy-option-mime-type',
 			voteEmitter.address,
 			VOTING_BLOCK,
 		])
@@ -28,8 +35,33 @@ describe('Vote', () => {
 	describe('constructor', () => {
 		it('Internal variables are recorded.', async () => {
 			const [vote, voteEmitter, , blockNUmber] = await init()
+			expect(await vote.subject()).to.be.equal('subject')
+			expect(await vote.body()).to.be.equal(
+				ethers.utils.formatBytes32String('body')
+			)
+			expect(await vote.options(0)).to.be.equal(options0)
+			expect(await vote.options(1)).to.be.equal(options1)
+			expect(await vote.bodyMimeType()).to.be.equal('dummy-body-mime-type')
+			expect(await vote.optionsMimeType()).to.be.equal('dummy-option-mime-type')
 			expect(await vote.period()).to.be.equal(blockNUmber + VOTING_BLOCK)
 			expect(await vote.voteEmitter()).to.be.equal(voteEmitter.address)
+		})
+	})
+	describe('attributes', () => {
+		it('Internal variables are recorded.', async () => {
+			const [vote, , , blockNUmber] = await init()
+			const attributes = await vote.attributes()
+			expect(attributes[0]).to.be.equal('subject')
+			expect(attributes[1]).to.be.equal(
+				ethers.utils.formatBytes32String('body')
+			)
+			expect((attributes[2] as BigNumber).toString()).to.be.equal(
+				(blockNUmber + VOTING_BLOCK).toString()
+			)
+			expect(attributes[3][0]).to.be.equal(options0)
+			expect(attributes[3][1]).to.be.equal(options1)
+			expect(attributes[4]).to.be.equal('dummy-body-mime-type')
+			expect(attributes[5]).to.be.equal('dummy-option-mime-type')
 		})
 	})
 	describe('vote', () => {
@@ -41,8 +73,6 @@ describe('Vote', () => {
 		})
 		it('save vote infomation.', async () => {
 			const [vote, voteEmitter, , , wallets] = await init()
-			const options0 = ethers.utils.formatBytes32String('http://hogehoge/0')
-			const options1 = ethers.utils.formatBytes32String('http://hogehoge/1')
 			const arg1 = [options0, options1]
 			await vote.vote(arg1, [40, 60])
 			const filterVote = voteEmitter.filters.Vote()
